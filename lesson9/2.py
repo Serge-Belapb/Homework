b965531d7ed77689c8d07c914c30eccb9057ff4a
Serge-Belapb <oostk203@gmail.com>
2024-05-08 14:28:52 +0300

    1

--- lesson9/2.py
+++ lesson9/2.py
@@ -5,3 +5,17 @@
 Пример: a = 5, b = 9 -> 35 (5+6+7+8+9)"
 
 '''
+
+
+n = 1
+
+
+def printn(text):
+    global n
+    for i in text:
+        print(n, text)
+        n += 1
+    return   
+
+
+print(printn(input("  :")))
