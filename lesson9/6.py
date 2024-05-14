05ea927c0a14e6704b85da9f81848b5f54d93dff
Serge-Belapb <oostk203@gmail.com>
2024-05-14 19:12:07 +0300

    up

---
+++ lesson9/6.py
@@ -0,0 +1,13 @@
+"""
+Дан словарь наблюдения за температурой 
+{"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}. 
+Отсортировать словарь по температуре в порядке возрастания и обратно.
+
+"""
+
+sl_temp = {"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}
+
+d = dict(sorted(sl_temp.items(), key=lambda x:x[1]))
+print(d)
+d = dict(sorted(sl_temp.items(), key=lambda x:x[1], reverse=True))
+print(d)
