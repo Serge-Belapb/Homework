bf82e4aad2e26876b85876862223e2728d756157
Serge-Belapb <oostk203@gmail.com>
2024-05-27 22:10:29 +0300

    up

--- lesson12/2.py
+++ lesson12/2.py
@@ -16,37 +16,91 @@
 from datetime import date
 
 CURRENT_YEAR = date.today().year
-print(CURRENT_YEAR)
 
-class BookCard():
-    author: str
-    title: str
-    year: int
+class BookCard:
+    def __init__(self, author, title, year):
+        self.__author = author    
+        self.__title = title      
+        self.__year = year       
+                  
+    def print_info(self):
+        print(f"Автор: {self.__author}\tНазвание: {self.__title}\tГод издания: {self.__year}")
 
-    def __init__(self, author, title, year) -> None:
-        self.author = author
-        self.title = title
-        self.year = year
-        
     def __le__(self, obj):
         return self.year <= CURRENT_YEAR
+
+    def __repr__(self):
+        return f"{self.__author} {self.__title} ({self.__year})"
+
+
+class Books:
+    lst = []
     
-    @property  # геттер
-    def year(self):
-        return self.year
+    @staticmethod
+    def add(book):
+        Books.lst.append(book)
+        
+    @staticmethod
+    def sort():
+        Books.lst.sort(key=lambda s: s.year)
+
+
+
+book1 = BookCard("Автор1", "Ноутбук", 2000)
+book2 = BookCard("Автор2", "Телефон", 1995),
+book3 = BookCard("Автор3", "Компьютер", 2019)
+book1.print_info()
+
+print(Books.lst)
+Books.add(book1)
+Books.add(book2)
+Books.add(book3)
+print(Books.lst)
+
+Books.sort()
+print(Books.lst)
     
-    @year.setter
-    def year(self, val):
-        if self.__le__(val) is isinstance(val, int):
-            self.year = val
-        else:
-            ValueError ("Не тот тип данных")
+    # @property  # геттер
+    # def author(self):
+    #     return self.author
+    # @property  # геттер
+    # def title(self):
+    #     return self.title
+    # @property  # геттер
+    # def year(self):
+    #     return self.year
+    
+    # @author.setter
+    # def author(self, other):
+    #     if self.author is isinstance(self.other, str):
+    #         self.author = self.o
+    #     else:
+    #         ValueError ("Не тот тип данных")
+    #     return self.author
+    
+    # @title.setter
+    # def title(self, other):
+    #     if self.author is isinstance(self.other, str):
+    #         self.author = self.o
+    #     else:
+    #         ValueError ("Не тот тип данных")
+    #     return self.author
+
+
+    
+    # @year.setter
+    # def year(self, ye):
+    #     if self.__le__(ye) is isinstance(ye, int):
+    #         self.year = ye
+    #     else:
+    #         ValueError ("Не тот тип данных")
 
 
 
-books = [
-    BookCard("Автор1", "Ноутбук", 2000),
-    BookCard("Автор2", "Телефон", 2005),
-    BookCard("Автор3", "Компьютер", 2019),
+# books = [
+#     BookCard("Автор1", "Ноутбук", 2000),
+#     BookCard("Автор2", "Телефон", 2005),
+#     BookCard("Автор3", "Компьютер", 2019),
          
-]
+# ]
+
---
+++ lesson12/3.py
@@ -0,0 +1,257 @@
+"""
+в файле hero1 добавить следующий функционал
+        - добавить несколько классов других героев унаследовав их от Hero.
+        - Каждому герою добавить уникальное свойство-спец.очки (мана, ярость, и т.п. ) и 
+                и свойство отражающее урон от спец.атаки.
+        - Создать метод атаки special_attack которая возможна только если количество 
+                спец.очков более 0.
+        - Добавить метод atack который при атаке с вероятностью 25% будет использовать 
+                спец.способность героя если у него остались спец.очки. 
+                При спец атаке вычитать из очков 1. Если вероятность пришлась на
+                остальные 75% - выполнить обычную атаку. Вывести сообщение в консоль 
+                о типе и результате атаки.
+
+добавить класс Arena:
+        - атрибут warriors - все воины на арене (тип list)
+        - магический метод __init__, который принимает необязательный аргумент warriors.
+                Если был передан список warriors, та заполняет им атрибут. Если нет, то заполняет
+                пустым списком.
+        - метод add_warrior, который принимает аргумент warrior и добавляет его к warriors.
+                Если данный воин уже есть в списке, то бросить исключение ValueError("Воин уже на арене").
+                Если нет, то добавить воина к списку warriors и вывести сообщение на экран
+                "{warrior.name} участвует в битве"
+        - метод choose_warrior, который не принимает аргументов и возвращает случайного
+                воина из warriors
+        - метод battle, который не принимает аргументов и симулирует битву. Сперва 
+                должна пройти проверка, что воинов на арене больше 1. Если меньше, то бросить
+                исключение ValueError("Количество воинов на арене должно быть больше 1").
+                Битва продолжается, пока на арене не останется только один воин. Сперва
+                в случайном порядке выбираются атакующий и защищающийся. Атакующий ударяет
+                защищающегося. Если у защищающегося осталось 0 health_points, то удалить его
+                из списка воинов и вывести на экран сообщение "{defender.name} пал в битве".
+                Когда останется только один воин, то вывести сообщение "Победил воин: {winner.name}".
+                Вернуть данного воина из метода battle.
+                
+                
+Создать несколько воинов используя разные классы, добавить их на арену и запустить битву. 
+Выжить должен только один.
+
+"""
+
+import random
+
+
+class Spell:
+    def __init__(self, name, dang=10, mana=5, type=1) -> None:
+        self.name = name
+
+class Hero:
+    """     
+    Класс дял создания героя 
+    
+     ...
+
+    Attributes
+    ----------
+    name : str
+        Имя героя
+    health : int
+        здоровье героя 
+    age : int
+        age of the person
+
+    Methods
+    -------
+    print_info():
+        Печатает в консоль информацию о герое
+    
+    kick():
+        производит один удар - высчитывает и уменьшает броню и здоровье 
+    
+    """
+    #  свойства класса - каждый созданный объект будет их иметь по умолчанию
+    option1 = True
+    points = 0
+    level = 1
+
+    # конструктор - тут мы создаем свойства которые должны быть у каждого нового объекта
+    # и присылаем сюда первоначальные их значения
+    def __init__(self, name, health, armor, strong) -> None:
+        # свойства объектов
+        self.name = name
+        self.health = health
+        self.armor = armor
+        self.strong = strong
+    
+    # методы - это действия/команды которые могут выполнять объекты
+    def _get_info(self):
+        print(
+              f"Имя {self.name}\n" \
+              f"Здоровье - {self.health}\n" \
+              f"Защита {self.armor}"
+        )
+        
+    def print_info(self):
+        
+        print(self._get_info(), '\n')
+    
+    def kick(self, other):        
+        other.armor -= self.strong
+        if other.armor < 0:
+            other.health += other.armor
+            other.armor = 0
+            
+    def special_attack(self, other):
+        other.armor -= (self.strong + self.spells)
+        if other.mana > 5 and other.armor < 0:
+            other.health += other.armor
+            other.armor = 0
+
+
+class Mag(Hero):    
+    def __init__(self, name, health, armor, strong, mana, spells) -> None:
+        # Hero.__init__(self, name, health, armor, strong)
+        super().__init__(name, health, armor, strong)
+        self.mana = mana
+        self.spells = spells
+        self.base_spell = fireball
+        
+
+
+    def cast_spell(self):
+        print(self.base_spell)
+    
+
+    def print_info(self, sep="-"):
+        info =  f"{super()._get_info()}\n"  \
+                f"{sep*20}\n" \
+                f"Мана - {self.mana}\n"
+        print(info)
+
+
+class Knight(Hero):
+    def __init__(self, name, health, armor, strong ) -> None:
+        super().__init__(name, health, armor, strong)
+
+
+class Archer(Hero):
+    def __init__(self, name, health, armor, strong, mana, skin, spells) -> None:
+        super().__init__(name, health, armor, strong)
+        self.mana = mana
+        self.spells = spells
+        self.base_spell = firebolt
+        self.skin = skin
+
+    def print_info(self, sep="-"):
+        info =  f"{sep*50}\n" \
+                f"{super()._get_info()}\n"  \
+                f"{sep*20}\n" \
+                f"Мана - {self.mana}\n"  \
+                f"Броня - {self.skin}\n"  \
+                f"Спец навык - {self.base_spell.name}"
+        print(info)    
+    
+    # def special_attack(self, other):
+    #     other.armor -= (self.strong + self.spells)
+    #     if other.mana > 5 and other.armor < 0:
+    #         other.health += other.armor
+    #         other.armor = 0
+
+    def attack(self, other):
+        r = random.randint(1, 100)
+        if r >= 75:
+            other.kick()
+            print(f'Тип атаки - обычная, твой шанс: {r}') 
+        elif r <= 25:
+            other.special_attack()
+            print(f'Тип атаки - специальная, твой шанс: {r}') 
+        
+        
+class Dragon(Hero):
+    def __init__(self, name, health, armor, strong, mana, skin, fury) -> None:
+        super().__init__(name, health, armor, strong)
+        self.mana = mana
+        self.fury = fury
+        self.skin = skin
+
+    def print_info(self, sep="-"):
+        info =  f"{sep*50}\n" \
+                f"{super()._get_info()}\n"  \
+                f"{sep*20}\n" \
+                f"Мана - {self.mana}\n"  \
+                f"Броня - {self.skin}\n"  \
+                f"Спец навык(ярость) - {self.fury}"
+        print(info)      
+    
+    def special_attack(self, other):
+        other.armor -= (self.strong + self.fury)
+        if other.mana > 15 and other.armor < 0:
+            other.health += other.armor
+            other.armor = 0
+
+
+
+
+class Arena:
+    def __init__(self, warriors: list) -> None:
+        self.warriors = warriors
+        if type(warriors) == list:
+            print(warriors)
+        else:
+            warriors = []
+            print("not a list.")
+        return warriors
+    
+    lst = []
+    
+    @staticmethod
+    def add_warrior(warr):
+        if warr not in Arena.lst:
+            Arena.lst.append(warr)
+        else:
+            ValueError("Воин уже на арене")
+        
+    def choose_warrior():
+        warrior_random = random.choice(Arena.lst)
+        print(warrior_random)
+        return warrior_random    
+
+    def battle():
+        if len(Arena.lst) > 2:
+            return True
+        else:
+            ValueError("Количество воинов на арене должно быть больше 1")
+
+        
+
+
+fireball = Spell('Fireball')         
+firebolt = Spell('Firebolt')        
+
+hero1 = Hero('Dimitri', 50, 20, 15)    
+hero2 = Hero('Alex', 60, 10, 5)    
+hero3 = Mag('Gendalf', 30, 25, 10, 30, [fireball])   
+hero4 = Archer("Elf", 40, 30, 5, 5, -5, [firebolt]) 
+hero5 = Dragon("Fafnir", 70, 40, 30, 120, -20, 20)
+
+# print(hero3.base_spell.name)
+# hero1.print_info()
+hero4.print_info()
+hero5.print_info()
+
+# print(hero4.mana)
+# print(hero3.mana)
+
+# hero4.attack(hero3)
+# hero5.print_info()
+# hero4.kick(hero3)
+# hero3.print_info()
+
+
+print(Arena.lst)
+Arena.add_warrior(hero1.name)
+Arena.add_warrior(hero2.name)
+Arena.add_warrior(hero3.name)
+Arena.add_warrior(hero4.name)
+Arena.add_warrior(hero5.name)
+print(Arena.lst)
