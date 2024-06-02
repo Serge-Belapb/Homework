860e327918802dca9dda88f8d227ea0028a59ed6
Serge-Belapb <oostk203@gmail.com>
2024-06-03 00:38:01 +0300

    up

--- lesson14/1.py
+++ lesson14/1.py
@@ -15,7 +15,7 @@ newTable = """CREATE TABLE IF NOT EXISTS users2(
 cur.execute(newTable)
 con.commit()
 
-sql = "INSERT INTO users (name, login, password, sub_date, sub_) " \
+sql = "INSERT INTO users (name, login, password, sub_date, sub_mode) " \
       "VALUES ('Иванов', 'Ivan_v', 'Iva0V', '12.11.2021', 'free')"
 
 # sql = "SELECT * FROM users WHERE name = 'uuu2'"
